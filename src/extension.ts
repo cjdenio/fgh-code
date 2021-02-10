e9f490c60bfc859b50574a7164d22948d5bd8856
Caleb Denio <cjdenio44@gmail.com>
2021-02-09 21:15:47 -0500

    ✨ Show list of cloned repos

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,18 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
+import {
+  commands,
+  env,
+  Uri,
+  window as vsCodeWindow,
+  ExtensionContext,
+} from "vscode";
 
-import { exec } from "child_process";
+import { getProjectList, getProjectPath } from "./fgh";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "fgh-code" is now active!');
@@ -14,28 +20,34 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
+  let disposable = commands.registerCommand(
     "fgh-code.openProject",
     async () => {
-      // The code you place here will be executed every time your command is executed
+      const projects = await getProjectList();
 
-      // Display a message box to the user
-      const text = await vscode.window.showInputBox({
-        placeHolder: "Project name",
-      });
+      const text = await vsCodeWindow.showQuickPick(Object.keys(projects));
 
-      if (text) {
-        exec(`fgh ls "${text}"`, async (err, stdout, stderr) => {
-          if (err) {
-            vscode.window.showErrorMessage("error :(");
-            return;
-          }
+      if (!text) {
+        return;
+      }
 
-          await vscode.commands.executeCommand(
-            "vscode.openFolder",
-            vscode.Uri.file(stdout.trim())
+      try {
+        await commands.executeCommand(
+          "vscode.openFolder",
+          Uri.file(projects[text])
+        );
+      } catch (err) {
+        console.error(err);
+        const clicked = await vsCodeWindow.showErrorMessage(
+          `error opening project: ${err.message}`,
+          "Install fgh"
+        );
+        if (clicked == "Install fgh") {
+          console.log("installing fgh");
+          env.openExternal(
+            Uri.parse("https://github.com/Matt-Gleich/fgh#readme")
           );
-        });
+        }
       }
     }
   );
---
+++ src/fgh.ts
@@ -0,0 +1,35 @@
+import { exec } from "child_process";
+
+export function getProjectPath(name: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    exec(`fgh ls "${name}"`, (err, stdout) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve(stdout.trim());
+    });
+  });
+}
+
+export function getProjectList(): Promise<{ [key: string]: string }> {
+  return new Promise((resolve, reject) => {
+    exec("fgh visualize --ownerName", (err, stdout) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      const lines = stdout.trim().split("\n");
+
+      const projects: { [key: string]: string } = {};
+
+      for (let i = 0; i < lines.length / 2; i++) {
+        projects[lines[i * 2]] = lines[i * 2 + 1];
+      }
+
+      resolve(projects);
+    });
+  });
+}
